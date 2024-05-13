2838655b7f49cd5a893435ddb8f8f5137a790881
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2024-05-13 18:19:10 -0300

    update Header component

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -2,13 +2,6 @@ import type { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 import Icon from "../components/ui/Icon.tsx";
 
-export interface CTA {
-  id?: string;
-  href: string;
-  text: string;
-  outline?: boolean;
-}
-
 export interface Nav {
   logo?: {
     src?: ImageWidget;
@@ -19,7 +12,6 @@ export interface Nav {
       label?: string;
       url?: string;
     }[];
-    buttons: CTA[];
   };
 }
 
@@ -36,59 +28,25 @@ export default function Header({
       { label: "Princing", url: "/" },
       { label: "Contact", url: "/" },
     ],
-    buttons: [
-      { id: "change-me-1", href: "/", text: "Change me", outline: false },
-      { id: "change-me-2", href: "/", text: "Change me", outline: true },
-    ],
   },
 }: Nav) {
   return (
-    <nav class="drawer drawer-end">
+    <nav class="absolute left-0 top-0 h-fit z-20 w-full box-border bg-cookie-cat bg-primary">
       <input id="mobile-drawer-nav" type="checkbox" class="drawer-toggle" />
 
       {/* main content */}
-      <div class="drawer-content container lg:px-0 px-4 flex gap-8 items-center justify-between py-4">
-        <a href="/">
-          <Image src={logo.src || ""} width={100} height={28} alt={logo.alt} />
-        </a>
-
-        <div class="hidden items-center justify-between lg:flex w-full">
-          <ul class="flex">
-            {navigation.links.map((link) => (
-              <li>
-                <a
-                  href={link.url}
-                  aria-label={link.label}
-                  class="link no-underline hover:underline p-4"
-                >
-                  {link.label}
-                </a>
-              </li>
-            ))}
-          </ul>
-          <ul class="flex gap-3">
-            {navigation.buttons?.map((item) => (
-              <a
-                key={item?.id}
-                id={item?.id}
-                href={item?.href ?? "#"}
-                target={item?.href.includes("http") ? "_blank" : "_self"}
-                class={`font-normal btn btn-primary ${
-                  item.outline && "btn-outline"
-                }`}
-              >
-                {item?.text}
-              </a>
-            ))}
-          </ul>
-        </div>
-
+      <div class="drawer-content container px-4 flex gap-8 items-center justify-between py-4">
         <label
           htmlFor="mobile-drawer-nav"
-          class="flex lg:hidden btn btn-ghost drawer-button"
+          class="flex btn btn-ghost drawer-button"
         >
           <Icon id="Bars3" size={24} strokeWidth={0.1} />
         </label>
+
+        <a href="/">
+          <Image class="h-fit max-w-16" src={logo.src || ""} alt={logo.alt} />
+        </a>
+
       </div>
 
       {/* sidebar */}
@@ -100,14 +58,9 @@ export default function Header({
           class="drawer-overlay"
         />
 
-        <div class="flex flex-col gap-8 min-h-full w-80 bg-base-100 text-base-content">
+        <div class="drawer flex flex-col gap-8 min-h-full w-80 bg-base-100 text-base-content bg-cookie-cat">
           <a class="p-4" href="/">
-            <Image
-              src={logo.src || ""}
-              width={100}
-              height={28}
-              alt={logo.alt}
-            />
+            <Image class="h-fit max-w-16" src={logo.src || ""} alt={logo.alt} />
           </a>
 
           <ul class="menu">
@@ -127,9 +80,8 @@ export default function Header({
                 id={item?.id}
                 href={item?.href ?? "#"}
                 target={item?.href.includes("http") ? "_blank" : "_self"}
-                class={`font-normal btn btn-primary ${
-                  item.outline && "btn-outline"
-                }`}
+                class={`font-normal btn btn-primary ${item.outline && "btn-outline"
+                  }`}
               >
                 {item?.text}
               </a>
